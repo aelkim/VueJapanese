556bc35d9df5818f4a27fd955406aeb7faa20f87
김아엘 <aelk2649@naver.com>
2024-03-13 07:07:59 +0900

    first commit

---
+++ vue-japanese-clone/src/Inquiry.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="inquiry--fixed shadow">
+    <i class="fa-brands fa-rocketchat"></i>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.inquiry--fixed {
+  width: 64px;
+  height: 64px;
+  border-radius: 50%;
+  position: fixed;
+  left: 17px;
+  bottom: 17px;
+  background-color: #f0f3f3;
+  visibility: hidden;
+  opacity: 0;
+  transition: all 1s ease-in-out;
+}
+.inquiry--fixed i {
+  font-size: 26px;
+  position: relative;
+  left: 29%;
+  bottom: -30%;
+}
+</style>
---
+++ vue-japanese-clone/src/Navigation.vue
@@ -0,0 +1,69 @@
+<template>
+  <nav class="nav-bar">
+    <div class="nav-bar__title-circle"></div>
+    <p class="nav-bar__title-words">PLAYLIVING IZU</p>
+    <ul class="nav-bar__list">
+      <li>Room</li>
+      <li>Experience</li>
+      <li>Food</li>
+      <li>Access</li>
+      <li>Reserve</li>
+      <li><i class="fa-brands fa-instagram"></i></li>
+    </ul>
+    <div class="nav-bar__empty"></div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.nav-bar {
+  width: 17vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  /* padding: 10px; */
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 100;
+}
+.nav-bar__empty {
+  width: 100%;
+  height: 25%;
+}
+.nav-bar__title-circle {
+  border-radius: 36%;
+  border: 1px solid black;
+  width: 65px;
+  height: 45px;
+  margin-top: 48px;
+}
+.nav-bar__title-words {
+  font-size: 13px;
+  font-weight: 350;
+  letter-spacing: 2px;
+  position: relative;
+  top: -67px;
+  left: 0;
+}
+.nav-bar__list {
+  list-style-type: none;
+  font-family: "Montserrat", sans-serif;
+  font-size: 90%;
+  font-weight: 350;
+  margin-top: 30px;
+}
+.nav-bar li {
+  margin-bottom: 10px;
+  letter-spacing: 1px;
+}
+.nav-bar li:last-child {
+  padding-top: 8px;
+  font-size: 15px;
+}
+</style>
